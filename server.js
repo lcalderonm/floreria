3757ad90a671bdcc30d0553f754339ea1e40f0b6
Luis Calderón <luis.calderonm@elektra.com.mx>
2022-09-25 21:11:43 -0500

    Join

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(serveStatic(path.join(__dirname, '/dist/index.html')));
 const port = process.env.PORT || 8080;
 app.listen(port);
 app.get(/.*/, function (req, res) {
-    res.sendFile(path.json(__dirname, '/dist/index.html'))
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 console.log(`app is listening on port: ${port}`)
