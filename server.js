ab604cf3038fa3ddf7be626cc006aa338e6b63ec
Luis Calderón <luis.calderonm@elektra.com.mx>
2022-09-25 20:56:35 -0500

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 3000;
+const app = express();
+app.use(serveStatic(path.join(__dirname, '/dist/index.html')));
+const port = process.env.PORT || 8080;
 app.listen(port);
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.json(__dirname, '/dist/index.html'))
+})
+console.log(`app is listening on port: ${port}`)
